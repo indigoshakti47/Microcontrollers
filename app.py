e90ce7efb1885f57a01d3b1b08e1e77b000410b8
Nathalia <indigodreamer11@hotmail.com>
2019-11-12 21:48:46 -0500

    First Commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template, url_for, flash, redirect, request, jsonify
+from flask_socketio import SocketIO, emit, send
+
+
+# Configuracion basica del servidor en Flask
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '9f3f918414c3b8f36c904aa6085d019c'
+app.debug = True
+app.host = '0.0.0.0'
+socketio = SocketIO(app)
+
+
+# Ruta por defecto en http://127.0.0.1:5000/
+@app.route("/")
+def hello():
+   return render_template('index.html')
+
+# SocketIO: aquí se manejan los mensajes entrantes
+# y salientes del servidor, este es el socket que
+# recibe las señales del archivo arduino.py
+start = True
+
+@socketio.on('proximity')
+def handle_proximity(message):
+   print('received message: ' + message)
+   send(message, broadcast=True)
+
+
+if __name__ == '__main__':
+   socketio.run(app)
---
+++ arduino.py
@@ -0,0 +1,45 @@
+# Aqui se importan las librerias necesarias para el
+# funcionamiento del programa, serial se utiliza para
+# la conexion con el dispositivo arduino y socketio
+# para la conexion con el servidor,
+import serial
+import socketio
+import time
+
+# En esta sección se realiza la conexion con los
+# Sockets del servidor remoto
+sio = socketio.Client()
+sio.connect('http://localhost:5000')
+
+# En esta sección se realiza la conexion con el
+# dispositivo arduino
+arduino = serial.Serial('/dev/cu.usbmodem14101',9600)
+counter = 0
+output,output_backup = [],[]
+
+
+
+# Aqui se comienza un bucle para recibir constantemente
+# las señales producidas por el arduino
+while True:
+
+    # rawString son las señales detectadas del arduino,
+    # vienen de la forma 'X-X-X-X-X-X-...X' donde X
+    # simboliza los bits producidos por cada uno de los sensores
+    rawString = str(arduino.readline(), 'utf-8').replace("\n", "")
+    if rawString is not None:
+        output.append(int(rawString.split("-")[0].replace("\r", "")))
+        counter += 1
+
+    if counter == 9:
+        if output_backup != output:
+            # si rawString ha cambiado de su posicion anterior,
+            # se entra a esta parte de codigo, se imprime en
+            # consola el codigo encontrado y se hace un HTTP
+            # request a los sockets del servidor remoto.
+            print('-'.join(str(e) for e in output))
+            sio.emit('message', '-'.join(str(e) for e in output))
+            output_backup = output
+        output = []
+        counter = 0
+arduino.close()
